74b8c1d35dcd5bd3fc7462d18729a445ac6cc0db
DZoltan <55488265+DZoltan@users.noreply.github.com>
2021-04-24 14:59:48 +0200

    Játéktér számításának előkészítése

---
+++ NI_Fonakolos_játék/NI_Fonakolos_játék/Model/BoardMesh.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NI_Fonakolos_játék.Model
+{
+    class BoardMesh
+    {
+        List<BoardTile> gameMesh = new List<BoardTile>();
+
+        public BoardMesh()
+        {
+
+        }
+
+        public void createTable()
+        {
+            int x = 0;
+            int y = 0;
+            for(int i = 0; i<8; i++)
+            {
+                y = 0;
+                x += 50;
+                for(int j = 0; j<8; j++)
+                {
+                    y += 50;
+                    gameMesh.Add(new BoardTile(x, y));
+                }
+            }
+            
+        }
+    }
+}
---
+++ NI_Fonakolos_játék/NI_Fonakolos_játék/Model/BoardTile.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace NI_Fonakolos_játék.Model
+{
+    class BoardTile
+    {
+        int pos_x { get; set; }
+        int pos_y { get; set; }
+        public int field_owner { get; set; }
+        //0=noOwner 1=white 2=black 3=whiteMust 4=blackMust
+
+        public BoardTile( int pos_x, int pos_y)
+        {
+            this.pos_x = pos_x;
+            this.pos_y = pos_y;
+            field_owner = 0;
+        }
+
+        public BoardTile() { }
+
+        public Point getPosition()
+        {
+            return new Point(pos_x, pos_y);
+        } 
+    }
+}
